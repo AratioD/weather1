0baea30a4b534ea126fd0050ca439cd5eacff499
AratioD <fffb@ddd.com>
2020-04-26 21:33:28 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('starting')
+
+
+setTimeout(() => {
+    console.log('2 second timer')
+}, 2000)
+
+setTimeout(() => {
+    console.log('o second timer')
+}, 0)
+
+
+console.log('stopping')
